bfd99bf35e1bcf22f323d45df1dd3e553b526f5f
Steve Thompson <writetome51@gmail.com>
2019-04-05 17:36:20 -0600

    minor changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 var notInteger = require("@writetome51/is-integer-is-float").notInteger;
 var errorIfNotFiniteNumber = require("error-if-not-finite-number").errorIfNotFiniteNumber;
 
 
 function errorIfNotInteger(arg) {
 	errorIfNotFiniteNumber(arg);
-	if (notInteger(arg))  throw new Error('Input must be integer');
+	if (notInteger(arg)) throw new Error('Input must be integer');
 }
 exports.errorIfNotInteger = errorIfNotInteger;
